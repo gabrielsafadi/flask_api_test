6ca28783a4dd05c68ef21adcbcfcbffe219da685
gabrielsafadi <86384045+gabrielsafadi@users.noreply.github.com>
2022-12-13 10:51:05 -0300

    Update bd.py

---
+++ bd.py
@@ -0,0 +1,17 @@
+Songs = [
+    {
+        'id': 1,
+        'song': 'high hopes',
+        'artist': 'kodaline'
+    },
+    {
+        'id': 2,
+        'song': 'iris',
+        'artist': 'the goo goo dolls'
+    },
+    {
+        'id': 3,
+        'song': 'oceans',
+        'artist': 'seafret'
+    }
+]
